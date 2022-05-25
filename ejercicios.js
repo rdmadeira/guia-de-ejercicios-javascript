70021d21ca799e2184705323a565d21d40a809f8
rdmadeira <rdmadeira2@gmail.com>
2022-05-25 15:04:36 -0300

    terminando 1 parte

--- ejercicios.js
+++ ejercicios.js
@@ -77,9 +77,57 @@ mayorNum(); */
 mayorNum(); */
 
 // Crear un programa que determine si un string introducido por un usuario empieza con un número o con una letra.
-numOLetra = () => {
+/* numOLetra = () => {
     const userString = prompt('Escriba su contraseña!','string');
-    userString[0].match(/[a-z]/) ? console.log('Su código empieza con una letra'): userString[0].match(/[0-9]/)? console.log(`Su código empieza con un numero.`): console.log('Ingrese un código con un numero o una letra')
+    userString[0].match(/[a-z]/gi) ? console.log('Su código empieza con una letra'): userString[0].match(/[0-9]/gi)? console.log(`Su código empieza con un numero.`): console.log('Ingrese un código con un numero o una letra')
     console.log(typeof userString[0]);
 }
-numOLetra();
+numOLetra(); */
+
+// Crear un programa donde se introduzcan los tres ángulos internos de un triángulo y se determine si el triángulo es válido o no.
+/* document.forms.form1.addEventListener('submit', 
+()=> {
+        const a1 = +(document.forms[0][0]['value']);
+        const a2 = +(document.forms[0][1]['value']);
+        const a3 = +(document.forms[0][2]['value']);
+        const suma = a1 + a2 + a3;
+        console.log(typeof a1, typeof a2, typeof a3)
+        console.log(suma)
+        if(suma === 180 && a1>0 && a2>0 && a3>0) { 
+            alert('El triangulo es válido');
+        } else{
+        alert('Los angulos no forman un triangulo');
+        }
+    }
+
+); */
+
+// Determinar si una palabra empieza con mayúscula o no.
+/* window.onload = () => {
+    const palabra = prompt('Verificar si una palabra empieza con una mayuscula','Escreva una palabra');
+    if (palabra[0].match(/[A-Z]/)) {
+        alert('Su palabra empieza con mayuscula');
+    } else {
+        alert('Su palabra no empieza con mayuscula')
+    }
+} */
+
+// Determinar si un año dado es bisiesto.
+/* let ano = prompt('Ingrese un año para saber si es bisiesto', 'Año');
+if(ano>=0 && ano%4===0 && (ano%100!==0 || ano%400===0)) {
+    alert(`El año de ${ano} ingresado es bisiesto`);
+ } else if(ano>=0) {
+    alert(`El año de ${ano} no es bisiesto.`)
+} else {
+    alert('Ingrese un año válido')
+} */
+
+// Escribir un programa JavaScript en el que el programa escoge al azar un entero entre 1 y 10, el usuario , luego a el usuario se le pedirá que introduzca un número en un popup para intentar adivinarlo. Si la entrada del usuario coincide con el número de conjetura, el programa mostrará un mensaje de "buen trabajo" de lo contrario mostrará un mensaje de "No corresponde"
+const randNum = Math.round(Math.random() * 10);
+console.log(randNum)
+miNum = prompt('Intente advinar el numero eligido','Numero de 1 al 10');
+if(+miNum === randNum) {
+    alert('Buen trabajo!');
+} else {
+    alert('No corresponde');
+}
